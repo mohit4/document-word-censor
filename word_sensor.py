1f7de381d0c4cfc57d2fa0a790b69ba3e7bc2873
mohit4 <mohitkumar2801@gmail.com>
2016-04-21 11:08:08 +0530

    Added command line argument feature

--- word_sensor.py
+++ word_sensor.py
@@ -3,49 +3,81 @@
 #getting the requirements
 import os
 import json
-from pprint import pprint
-
-#initializing data from json array
-file = open("words.json")
-data = json.load(file)
-
-#for records
-removed_words = []
-
-#getting the source
-source_fileName = "testfile.txt"
-destin_fileName = "modifile.txt"
-
-destin_fileObj = open(destin_fileName,"w")
-source_fileObj = open(source_fileName)
-source_text = source_fileObj.read()
-source_fileObj.close()
-
-#for debugging only
-#pprint(source_text)
+import sys
+#from pprint import pprint
 
 records = []
+data = None
 
-temp = ""
-for x in xrange(len(source_text)):
-	c = source_text[x]
-	if not c.isalnum():
-		destin_fileObj.write(temp)
-		destin_fileObj.write(c)
-		temp = ""
-	else:
-		temp+=c
+def sensor_func(source_text):
+	global records
+	global data
+	sensored_text = ""
 
-	if temp.lower() in data["words"]:
-		destin_fileObj.write("SENSORED")
-		records.append(temp)
-		temp = ""
+	temp = ""
+	for x in xrange(len(source_text)):
+		c = source_text[x]
+		if not c.isalnum():
+			sensored_text+=temp
+			sensored_text+=c
+			temp = ""
+		else:
+			temp+=c
 
-destin_fileObj.close()
+		if temp.lower() in data["words"]:
+			sensored_text+="SENSORED"
+			records.append(temp)
+			temp = ""
 
-os.remove(source_fileName)
-os.rename(destin_fileName,source_fileName)
+	return sensored_text
 
-print "Sensored the document : "+source_fileName
-pprint(records)
-print len(records),"words removed."
+def main():
+	
+	global records
+	global data
+	records = []
+
+	#initializing data from json array
+	file = open("sensor_words.json")
+	data = json.load(file)
+
+	#getting the source
+
+	if len(sys.argv) <= 1:
+		print "Error : No input file given"
+		sys.exit(1)
+
+	source_fileName = sys.argv[1]
+
+	if not os.path.isfile(source_fileName):
+		print "Error : No such file "+source_fileName
+		sys.exit(2)
+
+	destin_fileName = "temp"
+	if len(sys.argv) > 2:
+		destin_fileName = sys.argv[2]
+
+	source_fileObj = open(source_fileName)
+	source_text = source_fileObj.read()
+	source_fileObj.close()
+
+	#for debugging only
+	#pprint(source_text)
+
+	res_text = sensor_func(source_text)
+	destin_fileObj = open(destin_fileName,"w")
+	destin_fileObj.write(res_text)
+	destin_fileObj.close()
+
+	if destin_fileName == "temp":
+		os.remove(source_fileName)
+		os.rename(destin_fileName,source_fileName)
+
+	print "Sensored the document : "+source_fileName
+	#for debugging only
+	#print records
+	print len(records),"words removed."
+
+
+if __name__ == "__main__":
+	main()
